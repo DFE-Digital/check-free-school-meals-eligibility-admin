06665e1ff4b6f3ca6aaa0e1ac17e91f527398fed
YiannosGeorgantas <ioannis.georgantas@education.gov.uk>
2026-06-05 09:20:53 +0100

    Cypress changes

--- tests/cypress/e2e/Admin/AdminFullE2EJourney.spec.ts
+++ tests/cypress/e2e/Admin/AdminFullE2EJourney.spec.ts
@@ -152,7 +152,7 @@ describe('Full journey of creating an application through school portal through 
         //Approve Not Eligible Appeal Application from earlier
         cy.url().should('contain', 'Application/PendingApplications');
         cy.get('ul.govuk-pagination__list').find('li').last().find('a').click();
-        cy.scanPagesForNewValue(referenceNumber);
+        cy.scanPagesForNewValue(referenceNumber, 10);
         cy.contains('.govuk-button', 'Approve application').click();
         cy.contains('.govuk-button', 'Yes, approve now').click();
 
