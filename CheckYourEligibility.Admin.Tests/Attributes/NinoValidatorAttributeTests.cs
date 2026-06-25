63d6cd62e3c61c8b54646774a627557ca106cbbf
Jenna Barton <jenna.barton@education.gov.uk>
2026-06-25 17:54:01 +0100

    Clean up csproj and tests.csproj. Remove redundand tests from NinoValidatorAttributeTests

--- CheckYourEligibility.Admin.Tests/Attributes/NinoValidatorAttributeTests.cs
+++ CheckYourEligibility.Admin.Tests/Attributes/NinoValidatorAttributeTests.cs
@@ -9,9 +9,6 @@ public class NinoValidatorAttributeTests
 
     private class TestModel
     {
-        public string Selection { get; set; } = nameof(NinAsrSelect.None);
-
-        [NinValidator("Selection")]
         public string? NationalInsuranceNumber { get; set; }
     }
 
@@ -31,7 +28,6 @@ public class NinoValidatorAttributeTests
         // Arrange
         var model = new TestModel
         {
-            Selection = nameof(NinAsrSelect.NinSelected),
             NationalInsuranceNumber = nin
         };
 
@@ -62,7 +58,6 @@ public class NinoValidatorAttributeTests
         // Arrange
         var model = new TestModel
         {
-            Selection = nameof(NinAsrSelect.NinSelected),
             NationalInsuranceNumber = nin
         };
 
@@ -83,7 +78,6 @@ public class NinoValidatorAttributeTests
         // Arrange
         var model = new TestModel
         {
-            Selection = nameof(NinAsrSelect.NinSelected),
             NationalInsuranceNumber = "AB123456789012" // More than 9 characters
         };
 
@@ -100,72 +94,6 @@ public class NinoValidatorAttributeTests
 
     #endregion
 
-    #region Selection Logic Tests
-
-    [Test]
-    public void IsValid_WhenAsrnSelected_ReturnsSuccess()
-    {
-        // Arrange
-        var model = new TestModel
-        {
-            Selection = nameof(NinAsrSelect.AsrnSelected),
-            NationalInsuranceNumber = null
-        };
-
-        var context = new ValidationContext(model) { MemberName = nameof(TestModel.NationalInsuranceNumber) };
-        var results = new List<ValidationResult>();
-
-        // Act
-        var isValid = Validator.TryValidateProperty(model.NationalInsuranceNumber, context, results);
-
-        // Assert
-        Assert.That(isValid, Is.True);
-    }
-
-    [Test]
-    public void IsValid_WhenNoneSelected_ReturnsError()
-    {
-        // Arrange
-        var model = new TestModel
-        {
-            Selection = nameof(NinAsrSelect.None),
-            NationalInsuranceNumber = null
-        };
-
-        var context = new ValidationContext(model) { MemberName = nameof(TestModel.NationalInsuranceNumber) };
-        var results = new List<ValidationResult>();
-
-        // Act
-        var isValid = Validator.TryValidateProperty(model.NationalInsuranceNumber, context, results);
-
-        // Assert
-        Assert.That(isValid, Is.False);
-        Assert.That(results[0].ErrorMessage, Is.EqualTo("Please select one option"));
-    }
-
-    [Test]
-    public void IsValid_WhenNinSelectedButNull_ReturnsError()
-    {
-        // Arrange
-        var model = new TestModel
-        {
-            Selection = nameof(NinAsrSelect.NinSelected),
-            NationalInsuranceNumber = null
-        };
-
-        var context = new ValidationContext(model) { MemberName = nameof(TestModel.NationalInsuranceNumber) };
-        var results = new List<ValidationResult>();
-
-        // Act
-        var isValid = Validator.TryValidateProperty(model.NationalInsuranceNumber, context, results);
-
-        // Assert
-        Assert.That(isValid, Is.False);
-        Assert.That(results[0].ErrorMessage, Is.EqualTo("National Insurance number is required"));
-    }
-
-    #endregion
-
     #region Normalization Tests
 
     [Test]
@@ -174,7 +102,6 @@ public class NinoValidatorAttributeTests
         // Arrange
         var model = new TestModel
         {
-            Selection = nameof(NinAsrSelect.NinSelected),
             NationalInsuranceNumber = "AB 12 34 56 C"
         };
 
@@ -194,7 +121,6 @@ public class NinoValidatorAttributeTests
         // Arrange
         var model = new TestModel
         {
-            Selection = nameof(NinAsrSelect.NinSelected),
             NationalInsuranceNumber = "ab123456c"
         };
 
@@ -214,7 +140,6 @@ public class NinoValidatorAttributeTests
         // Arrange
         var model = new TestModel
         {
-            Selection = nameof(NinAsrSelect.NinSelected),
             NationalInsuranceNumber = "Ab 12 34 56 c"
         };
 
@@ -242,7 +167,6 @@ public class NinoValidatorAttributeTests
         // Arrange
         var model = new TestModel
         {
-            Selection = nameof(NinAsrSelect.NinSelected),
             NationalInsuranceNumber = nin
         };
 
@@ -262,7 +186,6 @@ public class NinoValidatorAttributeTests
         // Arrange - 8 characters without suffix
         var model = new TestModel
         {
-            Selection = nameof(NinAsrSelect.NinSelected),
             NationalInsuranceNumber = "AB123456"
         };
 
@@ -274,38 +197,6 @@ public class NinoValidatorAttributeTests
 
         // Assert
         Assert.That(isValid, Is.True);
-    }
-
-    #endregion
-
-    #region Edge Cases
-
-    [Test]
-    public void Constructor_WithNullSelectionPropertyName_ThrowsArgumentNullException()
-    {
-        // Act & Assert
-        Assert.Throws<ArgumentNullException>(() => new NinValidatorAttribute(null!));
-    }
-
-    [Test]
-    public void IsValid_WithInvalidSelectionProperty_ReturnsError()
-    {
-        // Arrange
-        var model = new TestModel
-        {
-            Selection = nameof(NinAsrSelect.NinSelected),
-            NationalInsuranceNumber = "AB123456C"
-        };
-
-        var context = new ValidationContext(model) { MemberName = nameof(TestModel.NationalInsuranceNumber) };
-        var attribute = new NinValidatorAttribute("NonExistentProperty");
-
-        // Act
-        var result = attribute.GetValidationResult(model.NationalInsuranceNumber, context);
-
-        // Assert
-        Assert.That(result, Is.Not.EqualTo(ValidationResult.Success));
-        Assert.That(result?.ErrorMessage, Does.Contain("Unknown property"));
     }
 
     #endregion
