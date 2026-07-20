4adf57e850430de51f089cc3d158de9ba82c203a
Jim Gregory <jim.gregory@advania.co.uk>
2026-07-20 12:03:19 +0100

    Fix parameter name for search status filtering in pagination view model

--- CheckYourEligibility.Admin/ViewModels/PaginationPartialViewModel.cs
+++ CheckYourEligibility.Admin/ViewModels/PaginationPartialViewModel.cs
@@ -10,7 +10,7 @@ public class PaginationPartialViewModel
     public int TotalRecords { get; set; }
     public string ControllerName { get; set; }
     public string? Keyword { get; set; }
-    public IEnumerable<string>? Status { get; set; }
+    public IEnumerable<string>? SelectedStatuses { get; set; }
     public string? DateFrom { get; set; }
     public bool? ShowArchived { get; set; }
 
