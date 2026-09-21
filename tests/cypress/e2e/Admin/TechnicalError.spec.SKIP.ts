28ad7e6ea989b9e1299cb82d7c57c78523921088
Jenna Barton <jenna.barton@education.gov.uk>
2026-09-21 10:45:53 +0100

    Update Perform check to Run check in additional files

--- tests/cypress/e2e/Admin/TechnicalError.spec.SKIP.ts
+++ tests/cypress/e2e/Admin/TechnicalError.spec.SKIP.ts
@@ -27,7 +27,7 @@ describe('TechnicalError outcome should display Error Code and CorrelationID', (
         cy.get('[id="DateOfBirth.Month"]').type('01');
         cy.get('[id="DateOfBirth.Year"]').type('1990');
         cy.get('#NationalInsuranceNumber').type("XX123456C");
-        cy.contains('button', 'Perform check').click();
+        cy.contains('button', 'Run Check').click();
 
         //Loader page
         cy.url().should('include', 'Check/Loader');
